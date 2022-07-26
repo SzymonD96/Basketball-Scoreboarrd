1083f73178f35da4ced211bed9815b01c0ee4c5c
Szymon Duda <s.duda996@gmail.com>
2022-07-26 21:51:47 +0200

    Fucntion improvement

--- index.js
+++ index.js
@@ -6,28 +6,20 @@ document.getElementById("home-points").textContent = homePoints;
 document.getElementById("guest-points").textContent = guestPoints;
 
 
-document.getElementById("plus1Home").addEventListener("click", function(){ addPoints("Home", 1) });
-document.getElementById("plus2Home").addEventListener("click", function(){ addPoints("Home", 2) });
-document.getElementById("plus3Home").addEventListener("click", function(){ addPoints("Home", 3) });
+document.getElementById("plus1Home").addEventListener("click", function(){ addPoints(true, 1) });
+document.getElementById("plus2Home").addEventListener("click", function(){ addPoints(true, 2) });
+document.getElementById("plus3Home").addEventListener("click", function(){ addPoints(true, 3) });
 
-document.getElementById("plus1Guest").addEventListener("click", function(){ addPoints("Guest", 1) });
-document.getElementById("plus2Guest").addEventListener("click", function(){ addPoints("Guest", 2) });
-document.getElementById("plus3Guest").addEventListener("click", function(){ addPoints("Guest", 3) });
+document.getElementById("plus1Guest").addEventListener("click", function(){ addPoints(false, 1) });
+document.getElementById("plus2Guest").addEventListener("click", function(){ addPoints(false, 2) });
+document.getElementById("plus3Guest").addEventListener("click", function(){ addPoints(false, 3) });
 
-function addPoints(side, pointAmmonut) {
-    let x = 10;
-    if (pointAmmonut === 2) {
-        x = 2;
-    } else if (pointAmmonut === 1) {
-        x = 1;
-    } else if (pointAmmonut === 3){
-        x = 3;
-    }
-    if (side === "Home") {
-        homePoints += x;
+function addPoints(isHome, pointAmmonut) {
+    if (isHome) {
+        homePoints += pointAmmonut;
+        document.getElementById("home-points").textContent = homePoints;
     } else {
-        guestPoints += x;
+        guestPoints += pointAmmonut;
+        document.getElementById("guest-points").textContent = guestPoints;
     }
-    document.getElementById("home-points").textContent = homePoints;
-    document.getElementById("guest-points").textContent = guestPoints;
 }
