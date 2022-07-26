8a38162e7147981a48905e1a1c6f46cb84172bb4
Szymon Duda <s.duda996@gmail.com>
2022-07-26 20:40:32 +0200

    Upgradeing function

--- index.js
+++ index.js
@@ -5,27 +5,29 @@ let guestPoints = 0;
 document.getElementById("home-points").textContent = homePoints;
 document.getElementById("guest-points").textContent = guestPoints;
 
-function plus1Home() {
-    homePoints++;
+
+document.getElementById("plus1Home").addEventListener("click", function(){ addPoints("Home", 1) });
+document.getElementById("plus2Home").addEventListener("click", function(){ addPoints("Home", 2) });
+document.getElementById("plus3Home").addEventListener("click", function(){ addPoints("Home", 3) });
+
+document.getElementById("plus1Guest").addEventListener("click", function(){ addPoints("Guest", 1) });
+document.getElementById("plus2Guest").addEventListener("click", function(){ addPoints("Guest", 2) });
+document.getElementById("plus3Guest").addEventListener("click", function(){ addPoints("Guest", 3) });
+
+function addPoints(side, pointAmmonut) {
+    let x = 10;
+    if (pointAmmonut === 2) {
+        x = 2;
+    } else if (pointAmmonut === 1) {
+        x = 1;
+    } else if (pointAmmonut === 3){
+        x = 3;
+    }
+    if (side === "Home") {
+        homePoints += x;
+    } else {
+        guestPoints += x;
+    }
     document.getElementById("home-points").textContent = homePoints;
-}
-function plus2Home() {
-    homePoints += 2;
-    document.getElementById("home-points").textContent = homePoints;
-}
-function plus3Home() {
-    homePoints += 3;
-    document.getElementById("home-points").textContent = homePoints;
-}
-function plus1Guest() {
-    guestPoints++;
-    document.getElementById("guest-points").textContent = guestPoints;
-}
-function plus2Guest() {
-    guestPoints += 2;
-    document.getElementById("guest-points").textContent = guestPoints;
-}
-function plus3Guest() {
-    guestPoints += 3;
     document.getElementById("guest-points").textContent = guestPoints;
 }
