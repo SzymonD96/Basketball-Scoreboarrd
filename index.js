c5a9b91605952a0a2c10355897e490eb7bd1e9fd
Szymon Duda <s.duda996@gmail.com>
2022-07-28 23:51:24 +0200

    last commit

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ document.getElementById("home-points").textContent = homePoints;
 document.getElementById("guest-points").textContent = guestPoints;
 
 
-document.getElementById("plus1Home").addEventListener("click", function(){ addPoints(true, 1) });
-document.getElementById("plus2Home").addEventListener("click", function(){ addPoints(true, 2) });
-document.getElementById("plus3Home").addEventListener("click", function(){ addPoints(true, 3) });
-
-document.getElementById("plus1Guest").addEventListener("click", function(){ addPoints(false, 1) });
-document.getElementById("plus2Guest").addEventListener("click", function(){ addPoints(false, 2) });
-document.getElementById("plus3Guest").addEventListener("click", function(){ addPoints(false, 3) });
+for(let i = 1; i <= 3; i++) {
+    document.getElementById("plus" + i + "Home").addEventListener("click", function(){ addPoints(true, i) });
+    document.getElementById("plus" + i + "Guest").addEventListener("click", function(){ addPoints(false, i) });
+}
 
 function addPoints(isHome, pointAmmonut) {
     if (isHome) {
